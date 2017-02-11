1d53e87cc7f541927e0301fd59453650b1bacf07
Steve Jewkes <steve@mulus.net>
2017-02-11 12:43:16 +0000

    Basic reading of rom file added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 
-int main( int argc, char **argv )
+/**
+ * @brief Main entry-point into application.
+ */
+int main(int argc, char **argv)
 {
 	std::cout << "Running jrnz..." << std::endl;
-	
+
+	if(argc < 2)
+	{
+		std::cerr << "Usage: " << argv[0] << " <rom image>" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::filebuf rom;
+	if(!rom.open(argv[1], std::ios::in | std::ios::binary))
+	{
+		std::cerr << "No file found called " << argv[1] << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	rom.close();
+
 	return EXIT_SUCCESS;
 }
