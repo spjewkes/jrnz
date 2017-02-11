c17d9f79d9998b3d9dbe40ce724636d98a4e34b0
Steve Jewkes <steve@mulus.net>
2017-02-11 15:16:34 +0000

    Added simple memory class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <vector>
+
+/**
+ * @brief Defines the memory of the device.
+ */
+class Memory
+{
+public:
+	Memory(size_t size, char *rom_file) : m_mem(size)
+		{
+			if(std::ifstream rom{rom_file, std::ios::binary | std::ios::ate})
+			{
+				auto rom_size = rom.tellg();
+				rom.seekg(0);
+				rom.read(reinterpret_cast<char*>(&m_mem[0]), rom_size);
+				rom.close();
+			}
+			else
+			{
+				std::cerr << "No file found called " << rom_file << std::endl;
+				std::cerr << "ROM uninitialized" << std::endl;
+			}
+		}
+
+private:
+	std::vector<unsigned char> m_mem;
+};
 
 /**
  * @brief Main entry-point into application.
@@ -15,19 +42,7 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	if(std::ifstream rom{argv[1], std::ios::binary | std::ios::ate})
-	{
-		auto size = rom.tellg();
-		std::string buffer(size, '\0');
-		rom.seekg(0);
-		rom.read(&buffer[0], size);
-		rom.close();
-	}
-	else
-	{
-		std::cerr << "No file found called " << argv[1] << std::endl;
-		return EXIT_FAILURE;
-	}
+	Memory mem(65536, argv[1]);
 
 	return EXIT_SUCCESS;
 }
