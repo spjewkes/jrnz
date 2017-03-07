64f964ab6b033cedbd5fe01e88cc3b6bd8bf8979
Steve Jewkes <steve@mulus.net>
2017-03-07 19:07:50 +0000

    Fix typo with add instruction string

--- src/z80.hpp
+++ src/z80.hpp
@@ -21,7 +21,7 @@ public:
 		{
 			map_inst.emplace(0x00, Instruction{InstType::NOP, "nop", 1, 4, Operand::UNUSED, Operand::UNUSED});
 			map_inst.emplace(0x11, Instruction{InstType::LD,  "ld de,**", 3, 10, Operand::DE, Operand::NN});
-			map_inst.emplace(0x19, Instruction{InstType::ADD, "add, hl, de", 1, 1, Operand::HL, Operand::DE});
+			map_inst.emplace(0x19, Instruction{InstType::ADD, "add hl, de", 1, 1, Operand::HL, Operand::DE});
 			map_inst.emplace(0x20, Instruction{InstType::JR,  "jr nz,*", 2, 12, 7, Conditional::NZ, Operand::PC, Operand::N});
 			map_inst.emplace(0x23, Instruction{InstType::INC, "inc hl", 1, 6, Operand::HL, Operand::ONE});
 			map_inst.emplace(0x28, Instruction{InstType::JR,  "jr z,*", 2, 12, 7, Conditional::Z, Operand::PC, Operand::N});
