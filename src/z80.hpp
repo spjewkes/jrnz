db9b82045185e6e542638479c5bcae24458cb47f
Steve Jewkes <steve@mulus.net>
2017-03-21 17:10:51 +0000

    Implemented another LD instruction

--- src/z80.hpp
+++ src/z80.hpp
@@ -23,8 +23,9 @@ public:
 			map_inst.emplace(0x01, Instruction{InstType::LD,  "ld bc,**", 3, 10, Operand::BC, Operand::NN});
 			map_inst.emplace(0x04, Instruction{InstType::INC, "inc b", 1, 4, Operand::B, Operand::ONE});
 			map_inst.emplace(0x11, Instruction{InstType::LD,  "ld de,**", 3, 10, Operand::DE, Operand::NN});
-			map_inst.emplace(0x19, Instruction{InstType::ADD, "add hl, de", 1, 1, Operand::HL, Operand::DE});
+			map_inst.emplace(0x19, Instruction{InstType::ADD, "add hl,de", 1, 1, Operand::HL, Operand::DE});
 			map_inst.emplace(0x20, Instruction{InstType::JR,  "jr nz,*", 2, 12, 7, Conditional::NZ, Operand::PC, Operand::N});
+			map_inst.emplace(0x21, Instruction{InstType::LD,  "ld hl,**", 3, 10, Operand::HL, Operand::NN});
 			map_inst.emplace(0x22, Instruction{InstType::LD,  "ld (**),hl", 3, 16, Operand::indNN, Operand::HL});
 			map_inst.emplace(0x23, Instruction{InstType::INC, "inc hl", 1, 6, Operand::HL, Operand::ONE});
 			map_inst.emplace(0x28, Instruction{InstType::JR,  "jr z,*", 2, 12, 7, Conditional::Z, Operand::PC, Operand::N});
