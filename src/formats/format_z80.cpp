0d6e56540ef25c24f3587e979772af993291d488
Steve Jewkes <steve@mulus.net>
2024-04-14 11:06:47 +0100

    Moved reading of first header into a separate function

--- src/formats/format_z80.cpp
+++ src/formats/format_z80.cpp
@@ -2,6 +2,7 @@
  * Implement reading of Z80 file format.
  */
 
+#include "bus.hpp"
 #include "z80.hpp"
 
 // TODO move this to a utility area
@@ -11,6 +12,9 @@ static uint8_t get_next_byte(std::ifstream &stream) {
     return static_cast<uint8_t>(ch);
 }
 
+/**
+ * @brief Read a data block from a Z80 file into memory.
+ */
 static void read_data_block(std::vector<uint8_t> &mem, std::ifstream &stream, bool compressed, uint16_t addr_start,
                             uint16_t size) {
     uint16_t mem_pos = addr_start;
@@ -56,6 +60,124 @@ static void read_data_block(std::vector<uint8_t> &mem, std::ifstream &stream, bo
     }
 }
 
+/**
+ * @brief Read the first header.
+ */
+void read_header_1(std::ifstream &stream, Z80 &state, Bus &bus, uint32_t &version, bool &compression_on) {
+    // 0x00 - AF
+    state.af.lo(get_next_byte(stream));
+    state.af.hi(get_next_byte(stream));
+
+    // 0x02 - BC
+    state.bc.lo(get_next_byte(stream));
+    state.bc.hi(get_next_byte(stream));
+
+    // 0x04 - HL
+    state.hl.lo(get_next_byte(stream));
+    state.hl.hi(get_next_byte(stream));
+
+    // 0x06 - PC
+    state.pc.lo(get_next_byte(stream));
+    state.pc.hi(get_next_byte(stream));
+    uint16_t pc = state.pc.get();
+    if (pc != 0x0000) {
+        version = 1;
+    }
+
+    // 0x08 - SP
+    state.sp.lo(get_next_byte(stream));
+    state.sp.hi(get_next_byte(stream));
+
+    // 0x0A - I
+    state.ir.hi(get_next_byte(stream));
+    // 0x0B - R - Note that top bit is stored in byte 12
+    state.ir.lo(get_next_byte(stream) & 0x7f);
+
+    // 0x0C - Byte 12
+    uint8_t byte_12 = get_next_byte(stream);
+    // Set top bit of R register
+    state.ir.lo(state.ir.lo() | ((byte_12 & 0x1) << 7));
+    // Get border color
+    uint8_t border_color = (byte_12 >> 1) & 0x7;
+    // Get SamRom Basic enabled
+    bool samrom_on = static_cast<bool>((byte_12 >> 4) & 0x1);
+    UNUSED(samrom_on);
+    // Get compression enabled or not
+    compression_on = static_cast<bool>((byte_12 >> 5) & 0x1);
+
+    if (byte_12 == 0xff) {
+        version = 1;
+    }
+
+    // Write border color to port 254
+    bus.port_254 &= 0xf8;
+    bus.port_254 |= border_color & 0x07;
+
+    // 0x0D - DE
+    state.de.lo(get_next_byte(stream));
+    state.de.hi(get_next_byte(stream));
+
+    // 0x0F - BC'
+    state.bc.swap();
+    state.bc.lo(get_next_byte(stream));
+    state.bc.hi(get_next_byte(stream));
+    state.bc.swap();
+
+    // 0x11 - DE'
+    state.de.swap();
+    state.de.lo(get_next_byte(stream));
+    state.de.hi(get_next_byte(stream));
+    state.de.swap();
+
+    // 0x13 - HL'
+    state.hl.swap();
+    state.hl.lo(get_next_byte(stream));
+    state.hl.hi(get_next_byte(stream));
+    state.hl.swap();
+
+    // 0x15 - AF'
+    state.af.swap();
+    state.af.lo(get_next_byte(stream));
+    state.af.hi(get_next_byte(stream));
+    state.af.swap();
+
+    // 0x17 - IY
+    state.iy.lo(get_next_byte(stream));
+    state.iy.hi(get_next_byte(stream));
+
+    // 0x19 - IX
+    state.ix.lo(get_next_byte(stream));
+    state.ix.hi(get_next_byte(stream));
+
+    // 0x1B - Interrupt flipflop, 0=DI, otherwise EI
+    bool interrupt_enabled = static_cast<bool>(get_next_byte(stream));
+    if (interrupt_enabled) {
+        state.iff1 = true;
+        state.iff2 = true;
+    }
+
+    // 0x1C - IFF2
+    uint8_t iff = get_next_byte(stream);
+    UNUSED(iff);
+
+    // 0x1D - Byte 29
+    uint8_t byte_29 = get_next_byte(stream);
+    // Interrupt mode 0, 1, or 2
+    state.int_mode = byte_29 & 0x3;
+    // Issue 2 enabled
+    bool issue2_enabled = static_cast<bool>((byte_29 >> 2) & 0x1);
+    UNUSED(issue2_enabled);
+    // Double interrupt frequency
+    bool double_interrupt_freq = static_cast<bool>((byte_29 >> 3) & 0x1);
+    UNUSED(double_interrupt_freq);
+    // Video synchronization
+    uint8_t video_sync = (byte_29 >> 4) & 0x3;
+    UNUSED(video_sync);
+    // Joystick selected
+    uint8_t joystick_selected = (byte_29 >> 6) & 0x3;
+    UNUSED(joystick_selected);
+}
+
 void Bus::load_z80(std::string &z80_file, Z80 &state) {
     if (std::ifstream z80{z80_file, std::ios::binary | std::ios::ate}) {
         uint32_t version = 0;
@@ -64,114 +186,8 @@ void Bus::load_z80(std::string &z80_file, Z80 &state) {
 
         z80.seekg(0);
 
-        // 0x00 - AF
-        state.af.lo(get_next_byte(z80));
-        state.af.hi(get_next_byte(z80));
-
-        // 0x02 - BC
-        state.bc.lo(get_next_byte(z80));
-        state.bc.hi(get_next_byte(z80));
-
-        // 0x04 - HL
-        state.hl.lo(get_next_byte(z80));
-        state.hl.hi(get_next_byte(z80));
-
-        // 0x06 - PC
-        state.pc.lo(get_next_byte(z80));
-        state.pc.hi(get_next_byte(z80));
-        uint16_t pc = state.pc.get();
-        if (pc != 0x0000) {
-            version = 1;
-        }
-
-        // 0x08 - SP
-        state.sp.lo(get_next_byte(z80));
-        state.sp.hi(get_next_byte(z80));
-
-        // 0x0A - I
-        state.ir.hi(get_next_byte(z80));
-        // 0x0B - R - Note that top bit is stored in byte 12
-        state.ir.lo(get_next_byte(z80) & 0x7f);
-
-        // 0x0C - Byte 12
-        uint8_t byte_12 = get_next_byte(z80);
-        // Set top bit of R register
-        state.ir.lo(state.ir.lo() | ((byte_12 & 0x1) << 7));
-        // Get border color
-        uint8_t border_color = (byte_12 >> 1) & 0x7;
-        // Get SamRom Basic enabled
-        bool samrom_on = static_cast<bool>((byte_12 >> 4) & 0x1);
-        UNUSED(samrom_on);
-        // Get compression enabled or not
-        bool compression_on = static_cast<bool>((byte_12 >> 5) & 0x1);
-
-        // Write border color to port 254
-        port_254 &= 0xf8;
-        port_254 |= border_color & 0x07;
-
-        // 0x0D - DE
-        state.de.lo(get_next_byte(z80));
-        state.de.hi(get_next_byte(z80));
-
-        // 0x0F - BC'
-        state.bc.swap();
-        state.bc.lo(get_next_byte(z80));
-        state.bc.hi(get_next_byte(z80));
-        state.bc.swap();
-
-        // 0x11 - DE'
-        state.de.swap();
-        state.de.lo(get_next_byte(z80));
-        state.de.hi(get_next_byte(z80));
-        state.de.swap();
-
-        // 0x13 - HL'
-        state.hl.swap();
-        state.hl.lo(get_next_byte(z80));
-        state.hl.hi(get_next_byte(z80));
-        state.hl.swap();
-
-        // 0x15 - AF'
-        state.af.swap();
-        state.af.lo(get_next_byte(z80));
-        state.af.hi(get_next_byte(z80));
-        state.af.swap();
-
-        // 0x17 - IY
-        state.iy.lo(get_next_byte(z80));
-        state.iy.hi(get_next_byte(z80));
-
-        // 0x19 - IX
-        state.ix.lo(get_next_byte(z80));
-        state.ix.hi(get_next_byte(z80));
-
-        // 0x1B - Interrupt flipflop, 0=DI, otherwise EI
-        bool interrupt_enabled = static_cast<bool>(get_next_byte(z80));
-        if (interrupt_enabled) {
-            state.iff1 = true;
-            state.iff2 = true;
-        }
-
-        // 0x1C - IFF2
-        uint8_t iff = get_next_byte(z80);
-        UNUSED(iff);
-
-        // 0x1D - Byte 29
-        uint8_t byte_29 = get_next_byte(z80);
-        // Interrupt mode 0, 1, or 2
-        state.int_mode = byte_29 & 0x3;
-        // Issue 2 enabled
-        bool issue2_enabled = static_cast<bool>((byte_29 >> 2) & 0x1);
-        UNUSED(issue2_enabled);
-        // Double interrupt frequency
-        bool double_interrupt_freq = static_cast<bool>((byte_29 >> 3) & 0x1);
-        UNUSED(double_interrupt_freq);
-        // Video synchronization
-        uint8_t video_sync = (byte_29 >> 4) & 0x3;
-        UNUSED(video_sync);
-        // Joystick selected
-        uint8_t joystick_selected = (byte_29 >> 6) & 0x3;
-        UNUSED(joystick_selected);
+        bool compression_on = false;
+        read_header_1(z80, state, *this, version, compression_on);
 
         if (version != 1) {
             std::cerr << "Only Z80 version 1 files are currently supported\n";
